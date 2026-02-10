d71b80a954c328f02a2089cde24d02f7413b1cc7
julien.bassin <8ee9627a76dab2c8a7@users.noreply.github.com>
2026-02-10 20:45:34 +0100

    feat: ignoring environments folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDiPjcQ0SJ3ImfTLioRJGl4g2mmumU8k6c",
+    authDomain: "my-awesome-app-production.firebaseapp.com",
+    projectId: "my-awesome-app-production",
+    storageBucket: "my-awesome-app-production.firebasestorage.app",
+    messagingSenderId: "693264796512",
+    appId: "1:693264796512:web:1dd98b8944cc657875ec05"
+  }
+};
