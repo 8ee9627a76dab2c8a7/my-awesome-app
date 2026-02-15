77cc62dd8cffa17b7f93e59ae98a4cba71d9ab35
julien.bassin <8ee9627a76dab2c8a7@users.noreply.github.com>
2026-02-15 09:40:49 +0100

    feat: homme-banner component done

--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { HomeBannerDumbComponent } from './home-banner/home-banner.dumb.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HomeBannerDumbComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss',
 })
 export class HomePageComponent {
+  title = 'La productivité au XXIème siècle';
+  description = "Atteignez plus d'objectif en moins de temps.";
+  button = 'Terminez vos journées en héros';
 
+  onBannerClicked(event: void): void {
+    console.log('Banner clicked!');
+    // TODO: Ajouter la logique de navigation ou d'action
+  }
 }
